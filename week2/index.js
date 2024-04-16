be6d7c34aa3f3edc32b08d138c8b07ec86ce0c67
namdaeun <nde40345@gmail.com>
2024-04-16 12:03:44 +0900

    feat: 상수로부터 쇼핑 아이템 렌더링

--- week2/index.js
+++ week2/index.js
@@ -1,3 +1,6 @@
+import { SHOPPING_LIST } from "./constants/shoppingList.js";
+
+const shoppingList = SHOPPING_LIST;
 const openModal = document.querySelector(".open_modal_btn");
 const modal = document.querySelector(".modal");
 
@@ -23,3 +26,45 @@ const items = document.querySelectorAll(".shopping_item");
 items.forEach((item) => {
   item.addEventListener("click", handleItemClick);
 });
+
+function filterItems() {
+  const categoryList = document.querySelectorAll(".category");
+
+  categoryList.forEach((category) =>
+    category.addEventListener("click", (event) => {
+      shoppingList.forEach((item) => {});
+    })
+  );
+}
+
+const renderItems = (shoppingList) => {
+  const itemLayout = document.querySelector(".item_layout");
+  itemLayout.innerHTML = "";
+
+  // 상수로부터 쇼핑 목록 렌더링
+  shoppingList.forEach((shoppingItem) => {
+    const item = document.createElement("article");
+    item.className = "shopping_item";
+    const img = document.createElement("img");
+    img.src = shoppingItem.image;
+    img.alt = shoppingItem.title;
+    const likeButton = document.createElement("button");
+    likeButton.className = "like_button";
+    const likeIcon = document.createElement("i");
+    likeIcon.className = "fa-regular fa-heart";
+    likeButton.appendChild(likeIcon);
+
+    const itemName = document.createElement("h4");
+    itemName.textContent = shoppingItem.title;
+    const itemPrice = document.createElement("p");
+    itemPrice.textContent = shoppingItem.price;
+
+    item.appendChild(img);
+    item.appendChild(likeButton);
+    item.appendChild(itemName);
+    item.appendChild(itemPrice);
+    itemLayout.appendChild(item);
+  });
+};
+
+renderItems(shoppingList);
