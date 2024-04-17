5371eaf509493f4974dd5353103cf6c305ac285b
namdaeun <nde40345@gmail.com>
2024-04-18 02:26:02 +0900

    feat: 장바구니 삭제 구현

--- week2/page/cart/renderCartList.js
+++ week2/page/cart/renderCartList.js
@@ -1,25 +1,39 @@
 const select = (selector) => document.querySelector(selector);
+const selectAll = (selector) => document.querySelectorAll(selector);
+
+let cartList = JSON.parse(sessionStorage.getItem("cartList")) || [];
+const cartTable = select(".cart_table");
+const cartBody = select(".cart_body");
 
 // 쇼핑 상품 렌더링
 export const renderCartList = () => {
-  const cartList = JSON.parse(sessionStorage.getItem("cartList"));
-  console.log(cartList);
-  const cartTable = select(".cart_table");
-  const renderCart = cartList.map((cartElement) => {
-    return `
-    <tr class="cart_row">
+  cartBody.innerHTML = "";
+  cartList.forEach((cartElement, index) => {
+    const cartRow = document.createElement("tr");
+    cartRow.classList.add("cart_row");
+    cartRow.innerHTML = `
       <td class="cart_data"><input type="checkbox" /></td>
-      <td class="cart_data"><img src=${
+      <td class="cart_data"><img src="${
         cartElement.image
-      } class="cart_image"></td>
+      }" class="cart_image"></td>
       <td class="cart_data">${cartElement.title}</td>
       <td class="cart_data">${Number(cartElement.price).toLocaleString()}</td>
       <td class="cart_data">${cartElement.category}</td>
-      <td class="cart_data"><button type="button">삭제</button></td>
-    </tr>
+      <td class="cart_data"><button type="button" class="delete_button" data-index="${index}">삭제</button></td>
     `;
+    cartBody.appendChild(cartRow);
   });
-  cartTable.innerHTML += renderCart.join("");
 };
 
 renderCartList();
+
+// 삭제 버튼 클릭 핸들러
+cartTable.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete_button")) {
+    // 클릭된 영역 중 삭제 버튼의 인덱스 저장
+    const index = parseInt(event.target.getAttribute("data-index"));
+    cartList = cartList.filter((item, i) => i !== index);
+    sessionStorage.setItem("cartList", JSON.stringify(cartList));
+    renderCartList();
+  }
+});
