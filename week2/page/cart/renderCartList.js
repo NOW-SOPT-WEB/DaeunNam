66d9b5eee2bcbc7a77aac00df136d598cbb321f5
namdaeun <nde40345@gmail.com>
2024-04-17 23:19:04 +0900

    feat: 선택한 상품 장바구니 페이지에 띄우기

---
+++ week2/page/cart/renderCartList.js
@@ -0,0 +1,23 @@
+const select = (selector) => document.querySelector(selector);
+
+// 쇼핑 상품 렌더링
+export const renderCartList = () => {
+  const cartList = JSON.parse(sessionStorage.getItem("cartList"));
+  console.log(cartList);
+  const cartTable = select(".cart_table");
+  const renderCart = cartList.map((cartElement) => {
+    return `
+    <tr class="cart_row">
+      <td class="cart_data"><input type="checkbox" /></td>
+      <td class="cart_data"><img src=${cartElement.image} class="cart_image"></td>
+      <td class="cart_data">${cartElement.title}</td>
+      <td class="cart_data">${cartElement.price}</td>
+      <td class="cart_data">${cartElement.category}</td>
+      <td class="cart_data"><button type="button">삭제</button></td>
+    </tr>
+    `;
+  });
+  cartTable.innerHTML += renderCart.join("");
+};
+
+renderCartList();
