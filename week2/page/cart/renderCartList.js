e030d914d6e446f83ac0effe7c8983f16da3c4e7
namdaeun <nde40345@gmail.com>
2024-04-18 01:52:58 +0900

    Revert "feat: 장바구니 상품 삭제 구현"
    This reverts commit 319c452174188b23dd2978fc7ee49fb331df46dd.

--- week2/page/cart/renderCartList.js
+++ week2/page/cart/renderCartList.js
@@ -1,37 +1,25 @@
 const select = (selector) => document.querySelector(selector);
-const selectAll = (selector) => document.querySelectorAll(selector);
-
-let cartList = JSON.parse(sessionStorage.getItem("cartList")) || [];
 
 // 쇼핑 상품 렌더링
 export const renderCartList = () => {
-  const cartTable = select(".cart_body");
-  cartTable.innerHTML = "";
-  cartList.forEach((cartElement, index) => {
-    const cartRow = document.createElement("tr");
-    cartRow.classList.add("cart_row");
-    cartRow.innerHTML = `
+  const cartList = JSON.parse(sessionStorage.getItem("cartList"));
+  console.log(cartList);
+  const cartTable = select(".cart_table");
+  const renderCart = cartList.map((cartElement) => {
+    return `
+    <tr class="cart_row">
       <td class="cart_data"><input type="checkbox" /></td>
-      <td class="cart_data"><img src="${
+      <td class="cart_data"><img src=${
         cartElement.image
-      }" class="cart_image"></td>
+      } class="cart_image"></td>
       <td class="cart_data">${cartElement.title}</td>
       <td class="cart_data">${Number(cartElement.price).toLocaleString()}</td>
       <td class="cart_data">${cartElement.category}</td>
-      <td class="cart_data"><button type="button" class="delete_button" data-index="${index}">삭제</button></td>
+      <td class="cart_data"><button type="button">삭제</button></td>
+    </tr>
     `;
-    cartTable.appendChild(cartRow);
   });
+  cartTable.innerHTML += renderCart.join("");
 };
 
 renderCartList();
-
-// 장바구니 상품 삭제
-select(".cart_table").addEventListener("click", (event) => {
-  if (event.target.classList.contains("delete_button")) {
-    const index = parseInt(event.target.getAttribute("data-index"));
-    cartList = cartList.filter((item, deleteIdx) => deleteIdx !== index);
-    sessionStorage.setItem("cartList", JSON.stringify(cartList));
-    renderCartList(); // 장바구니 다시 로드
-  }
-});
