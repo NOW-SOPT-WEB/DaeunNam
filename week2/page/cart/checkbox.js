006108160702b21603c63900935009223bc24f3a
namdaeun <nde40345@gmail.com>
2024-04-19 17:26:10 +0900

    feat: 장바구니 헤더 체크박스 클릭하면 체크박스 전체 선택됨

---
+++ week2/page/cart/checkbox.js
@@ -0,0 +1,18 @@
+const select = (selector) => document.querySelector(selector);
+const selectAll = (selector) => document.querySelectorAll(selector);
+
+let cartList = JSON.parse(sessionStorage.getItem("cartList")) || [];
+let checkedValues = [];
+
+// 헤더 체크박스 체크하면 하단 체크박스 전체 체크됨
+const checkboxes = selectAll('input[type="checkbox"]');
+const checkAllBoxes = (allCheckboxes) => {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = allCheckboxes.checked;
+  });
+};
+
+const headerCheckbox = select(".header_checkbox");
+headerCheckbox.addEventListener("click", () => {
+  checkAllBoxes(headerCheckbox);
+});
