33a8aeb65ec2bb5b79d17652c1a650f1f5fdb2cc
namdaeun <nde40345@gmail.com>
2024-04-19 18:20:34 +0900

    refactor: 새로운 파일로 나눈 코드 제거

--- week2/page/cart/renderCartList.js
+++ week2/page/cart/renderCartList.js
@@ -42,16 +42,3 @@ const deleteItem = () => {
   sessionStorage.setItem("cartList", JSON.stringify(cartList));
   renderCartList();
 };
-
-// 헤더 체크박스 체크하면 하단 체크박스 전체 체크됨
-const checkboxes = selectAll('input[type="checkbox"]');
-const checkAllBoxes = (allCheckboxes) => {
-  checkboxes.forEach((checkbox) => {
-    checkbox.checked = allCheckboxes.checked;
-  });
-};
-
-const headerCheckbox = select(".header_checkbox");
-headerCheckbox.addEventListener("click", () => {
-  checkAllBoxes(headerCheckbox);
-});
