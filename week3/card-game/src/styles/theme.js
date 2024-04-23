43e6a389551dad2267f030340b0324560cff58e6
namdaeun <nde40345@gmail.com>
2024-04-23 19:42:16 +0900

    init: 폰트 및 색상 추가

--- week3/card-game/src/styles/theme.js
+++ week3/card-game/src/styles/theme.js
@@ -1,10 +1,28 @@
-export const theme = {
+import { css } from "@emotion/react";
+
+const theme = {
   colors: {
     lightPurple: "#E9E3F8",
     gray: "#F5F5F7",
     black: "#010101",
+    white: "#FFFFFF",
   },
-  fonts: {},
+  fonts: {
+    title: css`
+      font-weight: 700;
+      font-size: 2.6rem;
+      font-family: "Pretendard Variable", sans-serif;
+      font-style: normal;
+      line-height: 100%;
+    `,
+    subTitle: css`
+      font-weight: 500;
+      font-size: 1.6rem;
+      font-family: "Pretendard Variable", sans-serif;
+      font-style: normal;
+      line-height: 100%;
+    `,
+  },
 };
 
 export default theme;
