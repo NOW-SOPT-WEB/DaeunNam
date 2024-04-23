35916f6b70eb0b208eb7b0246fc90afa88afa485
namdaeun <nde40345@gmail.com>
2024-04-23 19:50:55 +0900

    feat: main에 GlobalStyle과 Theme 적용

--- week3/card-game/src/main.jsx
+++ week3/card-game/src/main.jsx
@@ -1,15 +1,15 @@
-import { Global } from "@emotion/react";
-import React from "react";
+import { Global, ThemeProvider } from "@emotion/react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import globalStyle from "./styles/GlobalStyle.jsx";
+import theme from "./styles/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <Global styles={globalStyle} />
-    <React.StrictMode>
+    <ThemeProvider theme={theme}>
       <App />
-    </React.StrictMode>
+    </ThemeProvider>
   </>
 );
