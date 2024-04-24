a83a3be5062168b6eed2c86d90402969d5ea90b5
namdaeun <nde40345@gmail.com>
2024-04-24 18:00:40 +0900

    style: stage 버튼 스타일링 적용

--- week3/card-game/src/components/StageBtn.jsx
+++ week3/card-game/src/components/StageBtn.jsx
@@ -1,9 +1,19 @@
 import styled from "@emotion/styled";
 
 const StageBtn = (props) => {
-  return <BtnWrapper>{props.stage}</BtnWrapper>;
+  return <Stage>{props.stage}</Stage>;
 };
 
 export default StageBtn;
 
-const BtnWrapper = styled.section``;
+const Stage = styled.button`
+  padding: 0.5rem;
+  border: 2px solid ${({ theme }) => theme.colors.lightPurple};
+  border-radius: 0.8rem;
+  font-weight: bold;
+
+  &:hover {
+    transition: all 0.2s;
+    background-color: ${({ theme }) => theme.colors.lightPurple};
+  }
+`;
