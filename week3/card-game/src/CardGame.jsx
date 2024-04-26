7a4c67a99679a02dc0b9e5ac921062b50a5a646d
namdaeun <nde40345@gmail.com>
2024-04-27 03:07:20 +0900

    refactor: Header 컴포넌트 분리

--- week3/card-game/src/CardGame.jsx
+++ week3/card-game/src/CardGame.jsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import { useReducer, useState } from "react";
 import Card from "./components/Card";
+import Header from "./components/Header";
 import StageBtn from "./components/StageBtn";
 import { CARD_LIST } from "./constants/card";
 
@@ -47,13 +48,7 @@ const CardGame = () => {
 
   return (
     <GameWrapper>
-      <HeaderWrapper>
-        <TitleWithBtnContainer>
-          <Title>🐈 썬구리 고양이 맞추기 🐈‍⬛</Title>
-          <ResetBtn>Reset</ResetBtn>
-        </TitleWithBtnContainer>
-        <GameScore>0 / {cardPairsCount}</GameScore>
-      </HeaderWrapper>
+      <Header cardPairsCount={cardPairsCount} />
       <StageBtnWrapper>
         <StageBtn stage={"Easy"} onClick={() => dispatch({ type: "EASY" })} />
         <StageBtn
@@ -73,51 +68,6 @@ export default CardGame;
 
 const GameWrapper = styled.main`
   background-color: ${({ theme }) => theme.colors.gray};
-`;
-
-const HeaderWrapper = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 3rem;
-  background-color: ${({ theme }) => theme.colors.lightPurple};
-`;
-
-const TitleWithBtnContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-`;
-
-const Title = styled.h3`
-  display: flex;
-  padding: 2rem;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-  color: ${({ theme }) => theme.colors.black};
-  background-color: ${({ theme }) => theme.colors.lightPurple};
-
-  ${({ theme }) => theme.fonts.title};
-`;
-
-const ResetBtn = styled.button`
-  padding: 0.5rem;
-  border: 2px solid ${({ theme }) => theme.colors.lightPurple};
-  border-radius: 0.8rem;
-  font-weight: bold;
-
-  &:hover {
-    transition: all 0.3s;
-    background-color: ${({ theme }) => theme.colors.lightPurple};
-    border: 2px solid ${({ theme }) => theme.colors.purple};
-  }
-`;
-
-const GameScore = styled.h4`
-  ${({ theme }) => theme.fonts.title};
 `;
 
 const StageBtnWrapper = styled.section`
---
+++ week3/card-game/src/components/Header.jsx
@@ -0,0 +1,60 @@
+import styled from "@emotion/styled";
+
+const Header = ({ cardPairsCount }) => {
+  return (
+    <HeaderWrapper>
+      <TitleWithBtnContainer>
+        <Title>🐈 썬구리 고양이 맞추기 🐈‍⬛</Title>
+        <ResetBtn>Reset</ResetBtn>
+      </TitleWithBtnContainer>
+      <GameScore>0 / {cardPairsCount}</GameScore>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 3rem;
+  background-color: ${({ theme }) => theme.colors.lightPurple};
+`;
+
+const TitleWithBtnContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h3`
+  display: flex;
+  padding: 2rem;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  color: ${({ theme }) => theme.colors.black};
+  background-color: ${({ theme }) => theme.colors.lightPurple};
+
+  ${({ theme }) => theme.fonts.title};
+`;
+
+const ResetBtn = styled.button`
+  padding: 0.5rem;
+  border: 2px solid ${({ theme }) => theme.colors.lightPurple};
+  border-radius: 0.8rem;
+  font-weight: bold;
+
+  &:hover {
+    transition: all 0.3s;
+    background-color: ${({ theme }) => theme.colors.lightPurple};
+    border: 2px solid ${({ theme }) => theme.colors.purple};
+  }
+`;
+
+const GameScore = styled.h4`
+  ${({ theme }) => theme.fonts.title};
+`;
