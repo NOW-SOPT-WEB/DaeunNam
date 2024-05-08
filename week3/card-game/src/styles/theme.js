327406b51fd329fdd5a98af29f8f934fc3eff3bd
namdaeun <nde40345@gmail.com>
2024-05-08 13:40:18 +0900

    style: colors, fonts 따로 정의

--- week3/card-game/src/styles/theme.js
+++ week3/card-game/src/styles/theme.js
@@ -1,29 +1,31 @@
 import { css } from "@emotion/react";
 
-const theme = {
-  colors: {
-    purple: "#6139D1",
-    lightPurple: "#E9E3F8",
-    gray: "#F5F5F7",
-    black: "#010101",
-    white: "#FFFFFF",
-  },
-  fonts: {
-    title: css`
-      font-weight: 700;
-      font-size: 2.6rem;
-      font-family: "Pretendard Variable", sans-serif;
-      font-style: normal;
-      line-height: 100%;
-    `,
-    subTitle: css`
-      font-weight: 500;
-      font-size: 1.6rem;
-      font-family: "Pretendard Variable", sans-serif;
-      font-style: normal;
-      line-height: 100%;
-    `,
-  },
+const colors = {
+  purple: "#6139D1",
+  lightPurple: "#E9E3F8",
+  gray: "#F5F5F7",
+  black: "#010101",
+  white: "#FFFFFF",
 };
 
-export default theme;
+const fonts = {
+  title: css`
+    font-weight: 700;
+    font-size: 2.6rem;
+    font-family: "Pretendard Variable", sans-serif;
+    font-style: normal;
+    line-height: 100%;
+  `,
+  subTitle: css`
+    font-weight: 500;
+    font-size: 1.6rem;
+    font-family: "Pretendard Variable", sans-serif;
+    font-style: normal;
+    line-height: 100%;
+  `,
+};
+
+export const theme = {
+  colors,
+  fonts,
+};
