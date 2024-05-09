6413335a4b633c64f6039f58f4a42859e2d5be05
namdaeun <nde40345@gmail.com>
2024-05-10 04:04:48 +0900

    Feat: 메인페이지 생성

--- week4/login-signup-project/src/pages/Main/Main.tsx
+++ week4/login-signup-project/src/pages/Main/Main.tsx
@@ -1,5 +1,58 @@
+import styled from "@emotion/styled";
+import ReactPlayer from "react-player";
+import video from "../../assets/videos/forweber.mp4";
+
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <MainWrapper>
+      <VideoWrapper>
+        <ReactPlayer
+          url={video}
+          playing={true}
+          muted={true}
+          controls={false}
+          loop={true}
+          style={{
+            position: "absolute",
+            top: "30%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        />
+      </VideoWrapper>
+      <BtnWrapper>
+        <Button type="button">내 정보</Button>
+        <Button type="button">회원가입</Button>
+      </BtnWrapper>
+    </MainWrapper>
+  );
 };
 
 export default Main;
+
+const MainWrapper = styled.main`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.lightPurple};
+`;
+
+const VideoWrapper = styled.section`
+  position: relative;
+  height: 18rem;
+`;
+
+const BtnWrapper = styled.section`
+  display: flex;
+  gap: 3rem;
+  position: absolute;
+  top: 80%;
+`;
+
+const Button = styled.button`
+  padding: 0.3rem 0.5rem;
+  border-radius: 0.8rem;
+  border: 1px solid ${({ theme }) => theme.colors.black};
+`;
