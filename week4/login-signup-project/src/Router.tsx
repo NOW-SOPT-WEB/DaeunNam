67ce209e6248fd1c514c5f6a82c5fc611fb27c25
namdaeun <nde40345@gmail.com>
2024-05-10 03:20:16 +0900

    Feat: 마이페이지 생성

--- week4/login-signup-project/src/Router.tsx
+++ week4/login-signup-project/src/Router.tsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Join from './pages/Join/Join';
-import Login from './pages/Login/Login';
-import Main from './pages/Main/Main';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Join from "./pages/Join/Join";
+import Login from "./pages/Login/Login";
+import Main from "./pages/Main/Main";
+import MyPage from "./pages/MyPage/MyPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />}></Route>
-        <Route path="/main" element={<Main />}></Route>
+        <Route path="/main/:memberId" element={<Main />}></Route>
         <Route path="/join" element={<Join />}></Route>
+        <Route path="/member/:memberId" element={<MyPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
